7fe5b325745ca6a288169db01c7079419499621e
Victor Laskurain <victorlaskurain@gmail.com>
2023-09-17 20:23:09 +0200

    [IMP] Implement ScrumSprintBurndown SQL query.

--- addons/project_scrum/models/scrum_sprint.py
+++ addons/project_scrum/models/scrum_sprint.py
@@ -290,4 +290,46 @@ WHERE NOT ss.active
 class ScrumSprintBurndown(models.Model):
     _name = "scrum.sprint.burndown"
     _description = "Sprint Burndown Chart Data"
+    _order = "sprint_id, date"
     _auto = False
+
+    sprint_id = fields.Many2one("scrum.sprint", required=True, readonly=True)
+    date = fields.Date(required=True, readonly=True)
+    planned_hours = fields.Float(required=True, readonly=True)
+    available_hours = fields.Float(required=True, readonly=True)
+
+    def init(self):
+        self.env.cr.execute(
+            """
+DROP VIEW IF EXISTS scrum_sprint_burndown;
+CREATE VIEW scrum_sprint_burndown AS (
+    WITH sprint_reference AS (
+        SELECT sprint_id,
+               date,
+               SUM(dedication) AS hours
+        FROM scrum_sprint_developer_dedication_daily
+        GROUP BY sprint_id, date
+    ), sprint_estimation AS (
+        SELECT sprint_id,
+               date,
+               SUM(planned_hours) AS hours
+        FROM       scrum_sprint_task AS sst
+        INNER JOIN project_task_estimation_daily AS pted
+                ON sst.task_id = pted.task_id
+        GROUP BY sst.sprint_id, pted.date
+    )
+    SELECT reference.sprint_id,
+           reference.date,
+           estimation.hours AS planned_hours,
+           SUM(reference.hours) OVER (
+               PARTITION BY reference.sprint_id
+               ORDER BY reference.date DESC
+           ) AS available_hours
+    FROM       sprint_reference  AS reference
+    INNER JOIN sprint_estimation AS estimation
+            ON estimation.sprint_id = reference.sprint_id
+               AND estimation.date = reference.date
+    WHERE reference.hours > 0 -- remove any day with no activity.
+);
+"""
+        )
