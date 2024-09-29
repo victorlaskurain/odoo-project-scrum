57a4e800febe85a61b7fdc4571489ba3ddb15820
Victor Laskurain <victorlaskurain@gmail.com>
2024-09-29 11:48:39 +0200

    [FIX] project_scrum: fix imports.

--- addons/project_scrum/models/project_task.py
+++ addons/project_scrum/models/project_task.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Victor Laskurain
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 
 class Task(models.Model):
